66df2afaa153045ace98ecc1cb3f493d8ec8ceea
Jaremy Creechley <creechley@gmail.com>
2025-01-15 19:19:11 -0700

    expandpath

--- src/dmon/macos.nim
+++ src/dmon/macos.nim
@@ -5,6 +5,7 @@ import std/strutils
 import std/private/globs
 
 import logging
+import std/posix
 
 # MacOS-specific imports
 {.passL: "-framework CoreServices -framework CoreFoundation".}
@@ -280,7 +281,7 @@ proc watchDmon*(
   assert(dmonInitialized)
   assert(not rootdir.isEmptyOrWhitespace)
   assert(watchCb != nil)
-  let rootdir = rootdir.absolutePath
+  let rootdir = rootdir.absolutePath.expandFilename()
 
   notice "watchDmon: starting"
   # dmon.modifyWatches.store(1)
