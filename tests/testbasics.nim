94e43d567e641634a50f69bbe3eb5a471c06428b
Jaremy Creechley <creechley@gmail.com>
2025-01-28 22:30:47 -0700

    setting up test

--- tests/testbasics.nim
+++ tests/testbasics.nim
@@ -7,8 +7,50 @@
 
 import unittest
 
+import std/[times, os, strformat, strutils]
+
 import dmon
+import dmon/logging
+
+var fileModified = ""
 
 suite "dmon test":
   test "todo: test dmon init":
-    check false
+    let cb: WatchCallback = proc(
+        watchId: WatchId,
+        action: DmonAction,
+        rootDir, filepath, oldfilepath: string,
+        userData: pointer,
+    ) {.gcsafe.} =
+      {.cast(gcsafe).}:
+        echo "callback: ", "watchId: ", watchId.repr
+        echo "callback: ", "action: ", action.repr
+        echo "callback: ", "rootDir: ", rootDir.repr
+        echo "callback: ", "file: ", filepath.repr
+        echo "callback: ", "old: ", oldfilepath.repr
+        fileModified = filepath
+
+    initDmon()
+    startDmonThread()
+
+    echo("TEST: Starting...")
+    let watchId: WatchId = watch("./tests/", cb, {Recursive}, nil)
+    echo "TEST: watchId:repr: ", watchId.repr
+    let watchId2: WatchId = watch("./src/", cb, {Recursive}, nil)
+    echo("\nTEST: Waiting...")
+    os.sleep(500)
+    let testFile = "tests/file.txt".absolutePath
+    let now1 = now()
+    echo(fmt"TEST: Modifying {testFile} with {$now1}")
+    writeFile(testFile, $now1)
+    echo(fmt"Waiting...")
+
+    for i in 1..100:
+      if fileModified == "file.txt":
+        break
+      os.sleep(100)
+    check fileModified == "file.txt"
+
+    watchId.unwatch()
+    deinitDmon()
+
